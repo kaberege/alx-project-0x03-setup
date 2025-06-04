956482ab45e3303b491ceac7b6c32fe627e0f6b4
kaberege <kabgnestor@gmail.com>
2025-06-04 13:49:02 +0200

    added heading to the index.js

--- alx-project-0x03/pages/index.tsx
+++ alx-project-0x03/pages/index.tsx
@@ -4,7 +4,7 @@ import React from "react";
 const Home = () => {
   return (
     <div className="flex items-center justify-center text-2xl font-bold">
-      <h1 className=""> Welcome to Splash App </h1>
+      <h1> Welcome to Splash App </h1>
     </div>
   )
 }
