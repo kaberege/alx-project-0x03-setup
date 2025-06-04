4fe986448cc299a00fbfcc2254a28038bdaa55d1
kaberege <kabgnestor@gmail.com>
2025-06-04 13:44:23 +0200

    added google fonts

---
+++ alx-project-0x03/pages/index.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+
+const Home = () => {
+  return (
+    <div className="flex items-center justify-center text-2xl font-bold">
+      <h1 className=""> Welcome to Splash App </h1>
+    </div>
+  )
+}
+
+export default Home;
